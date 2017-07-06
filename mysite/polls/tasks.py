55d314c67cc20a15357c18c4d79c96e37733ab62
yrenyi <yrenyi0603@gmail.com>
2017-07-06 18:01:56 +0800

    update

--- mysite/polls/tasks.py
+++ mysite/polls/tasks.py
@@ -15,3 +15,15 @@ def mul(x, y):
 @task
 def xsum(numbers):
     return sum(numbers)
+from django.core.mail import send_mail
+from django.core.mail import EmailMessage
+@task
+def setemail(subject,message,from_email,recipient_list,cc_email):
+    email=EmailMessage(
+        subject,
+        message,
+        from_email,
+        recipient_list,
+        cc_email
+    )
+    email.send()
