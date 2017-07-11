45181cca5b445355c5534241943b9019c9d7f8e1
yrenyi <yrenyi0603@gmail.com>
2017-07-11 11:39:18 +0800

    update

--- mysite/polls/tasks.py
+++ mysite/polls/tasks.py
@@ -33,15 +33,15 @@ def masscan_check(hosts):
     logger.info('result is {0}'.format(out[0]))
     # from nmap import   nmap
     return list(set(hosts) - set(out[0].split()))
-
-def sendemail2(subject,message,from_email="",recipient_list="",cc_email=""):
+@task(bind=True,autoretry_for=(Exception,),ignore_result=True)
+def sendemail2(self,subject,message,from_email="",recipient_list="",cc_email=""):
     # backend = EmailBackend(host='mail.ims.cn', port=25, username='dop1@ims.cn', password='abc123!@#')
     from django.core.mail import  message as msg
     from_email = from_email or 'dop6@ims.cn'
     recipient_list = recipient_list or ['dop1@ims.cn']
     cc_email = cc_email or ['dop7@ims.cn']
     with mail.get_connection(host='mail.ims.cn', port=25, username='dop6@ims.cn', password='abc123!@#') as connection:
-        mail.EmailMessage(
+        msg=mail.EmailMessage(
             subject=subject,
             body=message,
             from_email=from_email,
@@ -53,15 +53,36 @@ def sendemail2(subject,message,from_email="",recipient_list="",cc_email=""):
                      # 'Importance':'High'
                      }
             # headers={"Message-ID":msg.make_msgid(idstring='yangry',domain='wosign.com')}
-        ).send()
+        )
+        msg.content_subtype="html"
+        msg.send()
 from .models import EmailcheckModel,PowercheckModel
 import json
-@task(bind=True,autoretry_for=(Exception,), max_retries=5,default_retry_delay=60*1,ignore_result=True)
+import datetime
+from django.template.loader import get_template
+# @task(bind=True,autoretry_for=(Exception,), max_retries=5,default_retry_delay=60*1,ignore_result=True)
+@task(bind=True)
 def sendEmailList(self):
-    emails=[i.email for i in EmailcheckModel.objects.all()]
-    sendemail2(subject='email list',message=json.dumps(emails))
-@task
-def sendIpAddress(ignore_result=True):
-    ip=[i.ipaddress for i in PowercheckModel.objects.filter(status__exact='Enable')]
-    # masscan_check(ip)
-    sendemail2(subject='ip list', message=json.dumps(masscan_check(ip)))
+    n=datetime.date.today()
+    emails= EmailcheckModel.objects.all()
+    template=get_template('emails/passwd_notice.html')
+
+    result=[]
+    for i in emails:
+        try:
+            d=(n-i.lastcgdate).days
+            if d >= 90-7 and d < 90:
+                sendemail2.delay(subject='提醒:您的邮箱密码即将过期',message=template.render({'email':i.email,'day':90-int(d)}))
+                logger.info('email:{0}'.format(i.email))
+                result.append(i.email)
+        except TypeError as e:
+            pass
+    logger.info('emails:{0}'.format(result))
+    # sendemail2.delay(subject='email list', message=json.dumps(result))
+    # sendemail2(subject='email list', message=json.dumps(result))
+
+# @task
+# def sendIpAddress(ignore_result=True):
+#     ip=[i.ipaddress for i in PowercheckModel.objects.filter(status__exact='Enable')]
+#     # masscan_check(ip)
+#     sendemail2(subject='ip list', message=json.dumps(masscan_check(ip)))
